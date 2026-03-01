6b40eba056438df90a6c04c2f425b1c02d93e20c
Ignacio Reguero <Ignacio.Reguero@cern.ch>
2026-03-01 18:04:16 +0100

    remove commented out code

--- producedfaggr.py
+++ producedfaggr.py
@@ -8,64 +8,6 @@ import matplotlib.pyplot as plt
 import os
 import pickle
 
-#for filename in ['RAW_data/PB5.acq', 'RAW_data/PB12.acq', 'RAW_data/PB13.acq', 'RAW_data/PB17.acq']:
-#    file = bioread.read_file(filename)
-
-
-#filename = 'RAW_data/PB17.acq'
-#filename = 'RAW_data/PB5.acq'
-#filename = 'RAW_data/PB12.acq'
-#filename = 'RAW_data/PB13.acq'
-#file = bioread.read_file(filename)
-#file = bioread.read_file('RAW_data/PB12.acq')
-#file = bioread.read_file('RAW_data/PB13.acq')
-
-#print(file)
-#print(file.channels)
-#print(file.named_channels)
-
-#freq_list = []
-#for channel in file.named_channels:
-#       freq_list.append(file.named_channels[channel].samples_per_second)
-#sampling_rate = np.max(freq_list)
-
-#data = {}
-#for channel in file.named_channels:
-#    print(file.named_channels[channel])
-#    print(dir(file.named_channels[channel]))
-#    print(file.named_channels[channel].name)
-#    print(file.named_channels[channel].units)
-#    print(file.named_channels[channel].data)
-#    signal = np.array(file.named_channels[channel].data)
-#    data[channel] = signal
-
-# Final dataframe
-#df = pd.DataFrame(data)
-#print(sampling_rate)
-#print(df)
-
-#print(file.event_markers)
-#sample_index = {}
-#for m in file.event_markers:
-#    #print(dir(m))
-#    #print(str(m))
-#    #print(str(m.sample_index))
-#    #ooprint(str(df[m.sample_index]))
-#    print(f"Marker '{m.text}' at sample {m.sample_index}")
-#    #print(f"Marker '{m.text}' at time {m.time_index}")
-#    #print('{0}: Channel {1}, type {2}'.format(m.text, m.channel_name, m.type))
-#    #sample_index[m.text] = m.sample_index
-#    #for channel in file.named_channels:
-#    #   print(file.named_channels[channel].data[m.sample_index])
-
-#data = {}
-#for channel in file.named_channels:
-#    signal = np.array(file.named_channels[channel].data[sample_index['start ABBA']:sample_index['end ABBA']])
-#    #signal = np.array(file.named_channels[channel].data[sample_index['start ABBA']:sample_index['ABBA end']])
-#    data[channel] = signal
-#ABBA_df = pd.DataFrame(data)
-#print(ABBA_df)
-
 class Segment(object):
     def __init__(self, name, start_index_text, start_index, seg_before):
         self.name = name
@@ -171,34 +113,6 @@ class Filedata(object):
                                                                                            
 def ECG_report(seg, name, fdat, fnam):
     print('Segment {0}.{1}:'.format(fnam, name))
-    #nk.signal_plot(seg.df, subplots=True, sampling_rate=1000)
-    #fig = plt.gcf()
-    #fig.savefig("all_{0}.png".format(name))
-    #plt.close()
-
-    #print('ECG of segment {0}.{1}:'.format(fnam, name))
-    #nk.signal_plot(seg.df['ECG (.5 - 35 Hz)'], sampling_rate=1000)
-    #fig = plt.gcf()
-    #fig.savefig("ecg_{0}.png".format(name))
-    #plt.close()
-    # Find peaks
-    #peaks, info = nk.ecg_peaks(seg.df['ECG (.5 - 35 Hz)'], sampling_rate=1000)
-    # Compute HRV indices
-    #hrv = nk.hrv_time(peaks, sampling_rate=1000, show=True)
-    #fig = plt.gcf()
-    #fig.savefig("hrv_{0}_{1}.png".format(fnam, name))
-    #plt.close(fig)
-    #print('HRV of segment {0}.{1}:'.format(fnam, name))
-    #print(hrv)
-    #for col in hrv:
-    #    print(col)
-    #print(hrv['HRV_MeanNN'])
-    #print(hrv.iloc[0]['HRV_MeanNN'])
-    #print(hrv['HRV_SDNN'])
-    #print(hrv.iloc[0]['HRV_SDNN'])
-    #print(hrv['HRV_RMSSD'])
-    #print(hrv.iloc[0]['HRV_RMSSD'])
-
     # Preprocess ECG signal
     clean_signals, info = nk.ecg_process(seg.df['ECG (.5 - 35 Hz)'], sampling_rate=1000)
     # Visualize
@@ -214,19 +128,12 @@ def ECG_report(seg, name, fdat, fnam):
     print('ECG analyze output of segment {0}.{1}:'.format(fnam, name))
     print(analyze_df)
     fdat.analyze_ecg = analyze_df
-    #if name =='baseline'or (name == 'stress2_ABBA' and fnam == 'PB1_only_part2'):
-    #    fdat.analyze_ecg = analyze_df
-    #else:
-    #    #fdat.analyze_ecg = fdat.analyze_ecg.append(analyze_df)
-    #    fdat.analyze_ecg = pd.concat([fdat.analyze_ecg, analyze_df], ignore_index=True)
-    #for col in analyze_df:
-    #    print(col)
-    print(analyze_df['HRV_MeanNN'].apply(lambda x: np.array(x).flatten()[0]))
-    #print(analyze_df['HRV_MeanNN'].iloc[0][0][0])
-    print(analyze_df['HRV_SDNN'].apply(lambda x: np.array(x).flatten()[0]))
-    #print(analyze_df.iloc[0]['HRV_SDNN'][0][0])
-    print(analyze_df['HRV_RMSSD'].apply(lambda x: np.array(x).flatten()[0]))
-    #print(analyze_df.iloc[0]['HRV_RMSSD'][0][0])
+    #print(analyze_df['HRV_MeanNN'].apply(lambda x: np.array(x).flatten()[0]))
+    ##print(analyze_df['HRV_MeanNN'].iloc[0][0][0])
+    #print(analyze_df['HRV_SDNN'].apply(lambda x: np.array(x).flatten()[0]))
+    ##print(analyze_df.iloc[0]['HRV_SDNN'][0][0])
+    #print(analyze_df['HRV_RMSSD'].apply(lambda x: np.array(x).flatten()[0]))
+    ##print(analyze_df.iloc[0]['HRV_RMSSD'][0][0])
 
 def EDA_report(seg, name, fdat, fnam):
     reportname = 'EDAreport_{0}_{1}.html'.format(fnam, name)
@@ -252,11 +159,6 @@ def EDA_report(seg, name, fdat, fnam):
     analyze_df['Sympathetic_Percent'] = analyze_df['EDA_SympatheticN'] * 100
     print(analyze_df)
     fdat.analyze_eda = analyze_df
-    #if name =='baseline'or (name == 'stress2_ABBA' and fnam == 'PB1_only_part2'):
-    #    fdat.analyze_eda = analyze_df
-    #else:
-    #    #fdat.analyze_eda = fdat.analyze_eda.append(analyze_df)
-    #    fdat.analyze_eda = pd.concat([fdat.analyze_eda, analyze_df], ignore_index=True)
 
 def Unlimited_duration_block(seg, name, fdat, fnam):
     unlimited_duration_sec = 0
@@ -273,18 +175,6 @@ def sort_filelist(l):
     return l
 
 def main():
-    #fdatas = {}
-    #labeled_files = os.listdir('RAW_data/labeled')
-    #for lbfile in sort_filelist(labeled_files):
-    #    if lbfile.endswith(".acq"):
-    #       filename = lbfile.removesuffix(".acq")
-    #       print(filename)
-    #       fdata = Filedata(filename)
-    #       fdata.preparedata()
-    #       fdatas[filename] = fdata
-    # Pickling (serializing) to a file
-    #with open('dataframes.pkl', 'wb') as f:
-    #    pickle.dump(fdatas, f)
 
     # Unpickling (deserializing) from a file
     with open('dataframes.pkl', 'rb') as f:
@@ -324,25 +214,13 @@ def main():
         pickle.dump(fdatas, f)
 
         print('\nFor file {0}:'.format(fnam))
+        # Please note that in Python 3.6+ dictionaries preserve insertion order.
         for name, seg in fdat.segments.items():
             if seg.marker_inside_index != 0:
                 print(seg.marker_inside_index)
                 print('segment: [{0}] before: [{1}] after [{2}] start [{3}] end [{4}] marker_inside label[{5}] marker_inside index [{6}]'.format(name, seg.before, seg.after, seg.start_index, seg.end_index, seg.marker_inside_text, seg.marker_inside_index))
             else:
                 print('segment: [{0}] before: [{1}] after [{2}] start [{3}] end [{4}]'.format(name, seg.before, seg.after, seg.start_index, seg.end_index))
-        #first = ""
-        #for name, seg in segments.items():
-        #    if seg.before == None:
-        #        first = name
-        #        break
-        #seg = segments[first]
-        #while True:
-        #    print(seg.name)
-        #    if seg.after == None:
-        #        break
-        #    seg = segments[seg.after]
-        #In Python 3.6+ dictionaries preserve insertion order.
-        #sys.exit(0)
     return 0
 
 if __name__ == "__main__":
